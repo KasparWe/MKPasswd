0d73cecc85ae066e46465242351683634b9d437c
Maximilian Volk <mav@tradebyte.biz>
2019-04-09 17:41:21 +0200

    Small fix to return encrypted password after encryption is done

--- includes/functions/actions.php
+++ includes/functions/actions.php
@@ -82,6 +82,7 @@ function encryptPasswd($username, $password)
     $enc_iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($cipher_method));
     $crypted_password = openssl_encrypt($password, $cipher_method, $enc_key, 0, $enc_iv) . "::" . bin2hex($enc_iv);
     unset($token, $cipher_method, $enc_key, $enc_iv);
+    return $crypted_password;
 }
 
 function decryptPasswd($username, $service)
